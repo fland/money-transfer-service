36ff11cbf4d2cf2fb8c6f530b984932de60aaea5
fland <fland@users.noreply.github.com>
2019-03-12 21:13:17 +0100

    + initial java maven skeleton

---
+++ src/main/java/org/money_transfer/service/Main.java
@@ -0,0 +1,15 @@
+package org.money_transfer.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Maksym Bondarenko
+ * @version 1.0 12.03.19
+ */
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
