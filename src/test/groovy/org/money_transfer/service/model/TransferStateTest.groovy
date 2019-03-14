afb553a22704f6317503da87ac18009f4f105d91
fland <fland@users.noreply.github.com>
2019-03-15 00:28:44 +0100

    + adjusted test with @Unroll

--- src/test/groovy/org/money_transfer/service/model/TransferStateTest.groovy
+++ src/test/groovy/org/money_transfer/service/model/TransferStateTest.groovy
@@ -1,6 +1,7 @@
 package org.money_transfer.service.model
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * @author Maksym Bondarenko
@@ -9,7 +10,8 @@ import spock.lang.Specification
 
 class TransferStateTest extends Specification {
 
-    def "should throw IllegalArgumentException on null values"() {
+    @Unroll
+    def "should throw IllegalArgumentException on null #argumentName"() {
         when:
         TransferState
                 .builder()
@@ -24,7 +26,11 @@ class TransferStateTest extends Specification {
         thrown IllegalArgumentException
 
         where:
-        uuid | sourceAccount | destinationAccount | amount | state
-        null | 123           | 321                | 12.3G  | TransferState.State.ACCEPTED
+        uuid    | sourceAccount | destinationAccount | amount | state                        | argumentName
+        null    | 123           | 321                | 12.3G  | TransferState.State.ACCEPTED | 'uuid'
+        'uuid1' | null          | 321                | 12.3G  | TransferState.State.ACCEPTED | 'sourceAccount'
+        'uuid1' | 123           | null               | 12.3G  | TransferState.State.ACCEPTED | 'destinationAccount'
+        'uuid1' | 123           | 321                | null   | TransferState.State.ACCEPTED | 'amount'
+        'uuid1' | 123           | 321                | 74.3G  | null                         | 'state'
     }
 }
